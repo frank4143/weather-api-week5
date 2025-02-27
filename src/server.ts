3e79576ed65529685a8286ff6a5addb4c8ad3bdb
frank4143 <131916960+frank4143@users.noreply.github.com>
2025-02-27 16:04:03 +0000

    Update Changes

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+ import express from "express";
+ import waetherRoute from"./routes/weatherRoutes.js";
+
+ // We will create an express app
+ const app = express();
+ 
+
+ // The port that the express server will listen on
+ const PORT = 3000;
+
+ app.use(express.json());
+
+ //our new route
+ app.use("/api/weather", waetherRoute);
+
+ // Start the express server
+ app.listen(PORT, () => {
+ console.log("Server is listening on port ${PORT}");
+ });
